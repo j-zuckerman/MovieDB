dbda4e9d19411e65e6d3ddd8f2b7d0b49f1a6ee7
James Zuckerman <jzuckerman4@gmail.com>
2019-02-16 15:34:43 -0700

    Updates

---
+++ precache-manifest.ad5cb76788d8b9d28753885f7f0d8977.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6fec01240c20f80e23c8",
+    "url": "/MovieDB/static/js/runtime~main.6fec0124.js"
+  },
+  {
+    "revision": "878f0417201f88d320a6",
+    "url": "/MovieDB/static/js/main.878f0417.chunk.js"
+  },
+  {
+    "revision": "a722b8be7e03d6e22f16",
+    "url": "/MovieDB/static/js/2.a722b8be.chunk.js"
+  },
+  {
+    "revision": "878f0417201f88d320a6",
+    "url": "/MovieDB/static/css/main.6a79b3ac.chunk.css"
+  },
+  {
+    "revision": "19d76cd19251a9eacb43ba4a679aa202",
+    "url": "/MovieDB/index.html"
+  }
+];
